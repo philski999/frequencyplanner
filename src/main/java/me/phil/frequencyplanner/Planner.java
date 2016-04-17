3fed3cbe8a67ce0221e422954a2831234d45e4e4
philski999 <phil.blenkinsopp@googlemail.com>
2016-04-17 20:56:52 +0100

    Added extra monitoring information.

--- src/main/java/me/phil/frequencyplanner/Planner.java
+++ src/main/java/me/phil/frequencyplanner/Planner.java
@@ -6,9 +6,13 @@ import java.io.InputStreamReader;
 
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
+import org.optaplanner.core.api.solver.event.BestSolutionChangedEvent;
+import org.optaplanner.core.api.solver.event.SolverEventListener;
 
 import me.phil.frequencyplanner.csvadapter.PlanningProblemReader;
 import me.phil.frequencyplanner.csvadapter.PlanningProblemWriter;
+import me.phil.frequencyplanner.domain.Channel;
+import me.phil.frequencyplanner.domain.FrequencyAssignment;
 import me.phil.frequencyplanner.domain.Plan;
 
 public class Planner {
@@ -25,16 +29,57 @@ public class Planner {
 					new InputStreamReader(channelsStream));
 		}        
        
-		System.out.println("Problem loaded, " + Integer.toString(unsolvedPlan.getFrequencyAssignments().size()) +
-				" assignments, " + Integer.toString(unsolvedPlan.getChannels().size()) +
-				" channels, solving...");
+		Integer availableChannelBandwidth = 0;
+		for (Channel channel : unsolvedPlan.getChannels()) {
+			availableChannelBandwidth += channel.getAvailableBandwidthInkHz();
+		}
+		
+		Integer bandwidthToAssign = 0 ;
+		for (FrequencyAssignment assignment : unsolvedPlan.getFrequencyAssignments()) {
+			bandwidthToAssign += assignment.getBandwidthInkHz();
+		}
+		
+		System.out.println("Problem loaded.");
+		System.out.println(Integer.toString(unsolvedPlan.getFrequencyAssignments().size()) + " assignments, " + 
+				Integer.toString(unsolvedPlan.getChannels().size()) + " channels, " +
+				availableChannelBandwidth.toString() + " available bandwidth (kHz), " +
+				bandwidthToAssign.toString() + " bandwidth to assign (kHz)");
+		System.out.println("Solving...");
 		
         // Solve the problem
-        solver.solve(unsolvedPlan);
+		SolverListener listener = new SolverListener();
+		solver.addEventListener(listener);
+		solver.solve(unsolvedPlan);
+		
         Plan solvedPlan = (Plan) solver.getBestSolution();
         
+		System.out.println("Solving complete, " + listener.getImprovementCount().toString() + " improvements made, " +
+				listener.getMillisToFindBest().toString() + "mS to find the best solution.");	
         System.out.println("Final score = " + solvedPlan.getScore());
 
         PlanningProblemWriter.print(solvedPlan, System.out);
     }
+    
+    /**
+     * Count the number of plan improvements made
+     */
+    private static class SolverListener implements SolverEventListener<Plan>
+	{
+		private Integer improvements = 0 ;
+		private Long milliSecondsToFindBest = -1L;
+		
+		@Override
+		public void bestSolutionChanged(BestSolutionChangedEvent<Plan> event) {
+			milliSecondsToFindBest = event.getTimeMillisSpent();
+			improvements++;
+		}
+		
+		Integer getImprovementCount() {
+			return improvements;
+		}
+		
+		Long getMillisToFindBest() {
+			return milliSecondsToFindBest;
+		}
+	}
 }
